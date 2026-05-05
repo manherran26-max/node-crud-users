da7d837438cdc6e9638a96b5d1ac7564ef6fdb84
mateo <manherran2.6@gmail.com>
2026-05-05 14:18:54 -0500

    agrego ruta base

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,18 +3,29 @@ import usersRoutes from "./src/routes/users.routes.js";
 import morgan from "morgan";
 import cors from "cors";
 import { PORT } from "./config.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+// Necesario para usar __dirname en ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// 👇 SERVIR FRONTEND
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+// 👇 RUTA PRINCIPAL
 app.get("/", (req, res) => {
-  res.send("API funcionando 🚀");
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
+// 👇 RUTAS API
 app.use(usersRoutes);
 
 app.listen(PORT);
