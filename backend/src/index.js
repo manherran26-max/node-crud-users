fedd43b613d97a5a3bebac1d643d16857080caba
mateo <manherran2.6@gmail.com>
2026-05-05 13:22:28 -0500

    agrego ruta base

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,11 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("API funcionando 🚀");
+});
+
 app.use(usersRoutes);
 
 app.listen(PORT);
